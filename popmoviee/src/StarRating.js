699489adb1b1441129dfceb7dc76aebdf8eb9577
unknown <bgsdewoo20@gmail.com>
2024-07-04 01:11:37 +0700

    Menampilkan Jumlah Bintang Sesuai Posisi Mouse

--- popmoviee/src/StarRating.js
+++ popmoviee/src/StarRating.js
@@ -24,6 +24,7 @@ const starStyle = {
 
 export default function StarRating({ max }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
   function handleRating(rating) {
     setRating(rating);
   }
@@ -34,17 +35,25 @@ export default function StarRating({ max }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating} stars</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
