37c0b3f6f8be7f507c0aa16ac2c8d94c06cefa83
unknown <bgsdewoo20@gmail.com>
2024-07-04 00:50:19 +0700

    Menampilkan Icon Bintang Dan Fungsi Klik Untuk Memberikan Nilai

--- popmoviee/src/index.js
+++ popmoviee/src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <StarRating max={5} />
     <StarRating max={10} />
   </React.StrictMode>
 );
